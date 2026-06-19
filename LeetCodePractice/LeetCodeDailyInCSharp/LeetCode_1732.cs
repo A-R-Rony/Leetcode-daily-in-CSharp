89c753ac9bfdf5ff3dd31960cb6313ebcc255481
BRACITS\abdur.rony <rhamanrony128@gmail.com>
2026-06-19 17:45:08 +0600

    daily added

---
+++ LeetCodePractice/LeetCodeDailyInCSharp/LeetCode_1732.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.LeetCodeDailyInCSharp;
+
+internal class LeetCode_1732
+{
+    public class Solution
+    {
+        public int LargestAltitude(int[] gain)
+        {
+            int mx = 0;
+            int last = 0;
+            foreach (var g in gain)
+            {
+                last += g;
+                mx = int.Max(last, mx);
+            }
+            return mx;
+        }
+    }
+}

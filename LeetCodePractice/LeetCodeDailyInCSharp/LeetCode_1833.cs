0a29226c4b97e02eda8c2dbcbb9ee5a95a63131c
BRACITS\abdur.rony <rhamanrony128@gmail.com>
2026-06-21 09:27:28 +0600

    daily added

---
+++ LeetCodePractice/LeetCodeDailyInCSharp/LeetCode_1833.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.LeetCodeDailyInCSharp;
+
+internal class LeetCode_1833
+{
+    public class Solution
+    {
+        public int MaxIceCream(int[] costs, int coins)
+        {
+            Array.Sort(costs);
+            for (int i = 0; i < costs.Length; i++)
+            {
+                if (coins >= costs[i])
+                {
+                    coins -= costs[i];
+                    continue;
+                }
+                else
+                {
+                    return i;
+                }
+            }
+            return costs.Length;
+
+        }
+    }
+}

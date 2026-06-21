b29ba9e505db309bb8297aad52964eaa0b200975
BRACITS\abdur.rony <rhamanrony128@gmail.com>
2026-06-21 09:26:45 +0600

    daily added

--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -8,11 +8,23 @@ namespace LeetCodePractice;
 
 public class Solution
 {
-    public double AngleClock(int hour, int minutes)
+    public int MaxIceCream(int[] costs, int coins)
     {
-        double H = hour * 30;
-        double M = (5.5) * minutes;
-        return Math.Min(Math.Abs(H - M), 360 - Math.Abs(H - M));
+        Array.Sort(costs);
+        for(int i = 0;i < costs.Length; i++)
+        {
+            if(coins >= costs[i])
+            {
+                coins -= costs[i];
+                continue;
+            }
+            else
+            {
+                return i;
+            }
+        }
+        return costs.Length;
+
     }
 }
 internal class Program
@@ -21,9 +33,9 @@ internal class Program
     {
         Solution solution = new Solution();
         string[] words = ["abcd", "def", "xyz"];
-        int[] weights = [5, 3, 12, 14, 1, 2, 3, 2, 10, 6, 6, 9, 7, 8, 7, 10, 8, 9, 6, 9, 9, 8, 3, 7, 7, 2];
+        int[] weights = [10, 6, 8, 7, 7, 8];
         string s = "z*#";
-        double result = solution.AngleClock(12,30);
+        int result = solution.MaxIceCream(weights,5);
         Console.WriteLine(string.Join(", ", result));
         Console.ReadLine();
     }

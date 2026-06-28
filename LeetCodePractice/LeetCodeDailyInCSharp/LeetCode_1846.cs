7125faf490b9e8fef1ffeb566f2b5db7bcc77eda
BRACITS\abdur.rony <rhamanrony128@gmail.com>
2026-06-28 10:40:06 +0600

    daily added

---
+++ LeetCodePractice/LeetCodeDailyInCSharp/LeetCode_1846.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodePractice.LeetCodeDailyInCSharp;
+
+internal class LeetCode_1846
+{
+    public class Solution
+    {
+
+        public int MaximumElementAfterDecrementingAndRearranging(int[] arr)
+        {
+            int n = arr.Length;
+            Array.Sort(arr);
+            int an = 1;
+            Console.Error.WriteLine($"x = {an}");
+            for (int i = 1; i < n; i++)
+            {
+                an = int.Min(an + 1, arr[i]);
+            }
+            return an;
+        }
+    }
+}
--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.Design;
+using System.Diagnostics;
 using System.Numerics;
 using System.Runtime.InteropServices.Marshalling;
 
@@ -7,59 +8,17 @@ namespace LeetCodePractice;
 
 public class Solution
 {
-    const int MAXN = (int)1e9;
-    public int MaximumLength(int[] nums)
+    
+    public int MaximumElementAfterDecrementingAndRearranging(int[] arr)
     {
-        Dictionary<int, int> cnt = new Dictionary<int, int>();
-        int an = nums.Count(x => x == 1);
-
-        foreach (var num in nums)
+        int n = arr.Length;
+        Array.Sort(arr);
+        int an = 1;
+        Console.Error.WriteLine($"x = {an}");
+        for (int i = 1;i < n;i++)
         {
-            if (!cnt.ContainsKey(num)) cnt[num] = 1;
-            else cnt[num]++;
+            an = int.Min(an + 1, arr[i]);
         }
-        foreach (var num in nums)
-        {
-            if (num == 1) continue;
-            int temp = num;
-            int rs = 0;
-            List<int> l = new List<int>();
-
-            while (temp <= MAXN)
-            {
-                if (!cnt.ContainsKey(temp)) break;
-                l.Add(temp);
-
-                if (MAXN / temp >= temp)
-                {
-                    temp *= temp;
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            //Console.WriteLine(string.Join(", ", l));
-            l.Sort();
-
-            for (int i = 0; i < l.Count; i++)
-            {
-                if (cnt[l[i]] >= 2)
-                {
-                    rs += 2;
-                }
-                else
-                {
-                    rs += 1;
-                    break;
-                }
-            }
-            if (rs % 2 == 0) rs--;
-            //Console.WriteLine($"rs: {rs}");
-            an = int.Max(an, rs);
-        }
-        if (an % 2 == 0) an--;
         return an;
     }
 }
@@ -69,9 +28,9 @@ internal class Program
     {
         Solution solution = new Solution();
          
-        int[] nums = [1, 1, 1, 1, 1];
+        int[] nums = [73, 98, 9];
         string s = "z*#";
-        int result = solution.MaximumLength(nums);
+        int result = solution.MaximumElementAfterDecrementingAndRearranging(nums);
         Console.WriteLine(string.Join(", ", result));
         Console.ReadLine();
     }

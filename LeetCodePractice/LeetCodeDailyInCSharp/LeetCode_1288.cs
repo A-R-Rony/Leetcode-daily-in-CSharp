781307b316c9c9c8b2a9e38851ac9e1bc368a207
BRACITS\abdur.rony <rhamanrony128@gmail.com>
2026-07-06 10:28:28 +0600

    daily added

---
+++ LeetCodePractice/LeetCodeDailyInCSharp/LeetCode_1288.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCodePractice.LeetCodeDailyInCSharp;
+
+internal class LeetCode_1288
+{
+    public class Solution
+    {
+        public int RemoveCoveredIntervals(int[][] intervals)
+        {
+            int n = intervals.Length;
+            int an = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                bool ok = false;
+                for (int j = 0; j < n; j++)
+                {
+                    if (j == i) continue;
+                    var a = intervals[i];
+                    var b = intervals[j];
+
+                    if (b[0] <= a[0] && a[1] <= b[1])
+                    {
+                        ok = true;
+                    }
+                }
+                if (!ok)
+                {
+                    an++;
+                }
+            }
+            return an;
+        }
+    }
+}
--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -5,49 +5,34 @@ using System.Numerics;
 using System.Runtime.InteropServices.Marshalling;
 
 namespace LeetCodePractice;
+
 public class Solution
 {
-    int[] dx = [1, -1, 0, 0];
-    int[] dy = [0, 0, -1, 1];
-    int[,,] vis;
-    int n, m;
-    bool isValid(int x, int y)
+    public int RemoveCoveredIntervals(int[][] intervals)
     {
-        return (x >= 0 && x < n && y >= 0 && y < m);
-    }
-    bool Dfs(int x, int y, IList<IList<int>> grid, int h)
-    {
-        if (x == n - 1 && y == m - 1)
+        int n = intervals.Length;
+        int an = 0;
+
+        for(int i = 0;i < n; i++)
         {
-            if (grid[x][y] == 1) return (h >= 2);
-            return (h >= 1);
+            bool ok = false;
+            for(int j = 0;j < n;j++)
+            {
+                if (j == i) continue;
+                var a = intervals[i];
+                var b = intervals[j];
+
+                if (b[0] <= a[0] && a[1] <= b[1])
+                {
+                    ok = true;
+                }
+            } 
+            if(!ok)
+            {
+                an++;
+            }
         }
-        vis[x, y, h] = 1;
-        bool ok = false;
-
-        for (int i = 0; i < 4; i++)
-        {
-            int tx = x + dx[i];
-            int ty = y + dy[i];
-            if (!isValid(tx, ty)) continue;
-            int nh = h - (grid[x][y] == 1 ? 1 : 0);
-            if (nh < 1) continue;
-
-            if (vis[tx, ty, nh] != 0) continue;
-
-
-            ok |= Dfs(tx, ty, grid, nh);
-        }
-        return ok;
-
-    }
-    public bool FindSafeWalk(IList<IList<int>> grid, int health)
-    {
-        n = grid.Count();
-        m = grid[0].Count();
-
-        vis = new int[n, m, n + m + 1];
-        return Dfs(0, 0, grid, health);
+        return an;
     }
 }
 internal class Program
@@ -57,15 +42,12 @@ internal class Program
         Solution solution = new Solution();
 
         int[] nums = [73, 98, 9];
-        IList<IList<int>> grid = new List<IList<int>>()
-{
-    new List<int> { 0, 0, 0, 1 },
-    new List<int> { 0, 0, 0, 0 },
-    new List<int> { 0, 0, 0, 0 },
-    new List<int> { 1, 0, 0, 0 }
-};
+        int[][] edges = [[1, 4], [2, 3]];
+        bool[] online = [true, true, true, false, true];
+
+
         string s = "z*#";
-        bool result = solution.FindSafeWalk(grid, 2);
+        int result = solution.RemoveCoveredIntervals(edges);
         Console.WriteLine(string.Join(", ", result));
         Console.ReadLine();
     }

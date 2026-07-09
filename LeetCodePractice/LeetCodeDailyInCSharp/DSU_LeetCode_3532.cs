547b4556230d59fcb576a20c39984c989b5f8895
BRACITS\abdur.rony <rhamanrony128@gmail.com>
2026-07-09 10:04:27 +0600

    daily added

---
+++ LeetCodePractice/LeetCodeDailyInCSharp/DSU_LeetCode_3532.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.LeetCodeDailyInCSharp;
+
+internal class DSU_LeetCode_3532
+{
+    public class Dsu
+    {
+        int[] Parent;
+        int N;
+        public Dsu(int n)
+        {
+            N = n;
+            Parent = new int[N + 1];
+            for (int i = 0; i <= N; i++)
+            {
+                Parent[i] = i;
+            }
+        }
+
+        public int Find(int u)
+        {
+            if (Parent[u] == u) return u;
+            return Parent[u] = Find(Parent[u]);
+        }
+        public void Union(int u, int v)
+        {
+            Parent[v] = u;
+        }
+    }
+    public class Solution
+    {
+        int[] Nums;
+        int N, MaxDiff;
+        List<int>[] G;
+        int[] Parents;
+        int UpperBound(int x)
+        {
+            int l = 0, r = N - 1, an = (x >= Nums[N - 1] ? N : -1);
+            while (l <= r)
+            {
+                int md = l + (r - l) / 2;
+                if (Nums[md] <= x)
+                {
+                    an = md;
+                    l = md + 1;
+                }
+                else
+                {
+                    r = md - 1;
+                }
+            }
+            return an;
+        }
+        public bool[] PathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries)
+        {
+            this.N = n;
+            this.Nums = nums;
+            this.MaxDiff = maxDiff;
+            G = new List<int>[n + 1];
+            Parents = new int[n + 1];
+
+            for (int i = 0; i <= n; i++)
+            {
+                Parents[i] = i;
+            }
+
+            for (int i = 0; i <= n; i++)
+            {
+                G[i] = new List<int>();
+            }
+
+            Dsu dsu = new Dsu(N);
+
+            for (int i = 0; i < n; i++)
+            {
+                int ub = UpperBound(nums[i] + maxDiff);
+
+                int pi = dsu.Find(i);
+                int pub = dsu.Find(ub);
+
+                if (pi != pub)
+                {
+                    dsu.Union(pi, pub);
+                }
+            }
+
+            int m = queries.Length;
+            bool[] an = new bool[m];
+
+            for (int i = 0; i < m; i++)
+            {
+                int u = queries[i][0];
+                int v = queries[i][1];
+
+                if (dsu.Find(u) == dsu.Find(v)) an[i] = true;
+                else an[i] = false;
+            }
+            return an;
+
+        }
+    }
+}
--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -6,33 +6,100 @@ using System.Runtime.InteropServices.Marshalling;
 
 namespace LeetCodePractice;
 
+public class Dsu
+{
+    int[] Parent;
+    int N;
+    public Dsu(int n)
+    {
+        N = n;
+        Parent = new int[N+1];
+        for (int i = 0; i <= N; i++)
+        {
+            Parent[i] = i;
+        }
+    }
+
+    public int Find(int u)
+    {
+        if (Parent[u] == u) return u;
+        return Parent[u] = Find(Parent[u]);
+    }
+    public void Union(int u, int v)
+    {
+        Parent[v] = u;
+    }
+}
 public class Solution
 {
-    public int RemoveCoveredIntervals(int[][] intervals)
+    int[] Nums;
+    int N, MaxDiff;
+    List<int>[] G;
+    int[] Parents;
+    int UpperBound(int x)
     {
-        int n = intervals.Length;
-        int an = 0;
-
-        for(int i = 0;i < n; i++)
+        int l = 0, r = N - 1, an = (x >= Nums[N - 1] ? N : -1);
+        while (l <= r)
         {
-            bool ok = false;
-            for(int j = 0;j < n;j++)
+            int md = l + (r - l) / 2;
+            if (Nums[md] <= x)
             {
-                if (j == i) continue;
-                var a = intervals[i];
-                var b = intervals[j];
-
-                if (b[0] <= a[0] && a[1] <= b[1])
-                {
-                    ok = true;
-                }
-            } 
-            if(!ok)
+                an = md;
+                l = md + 1;
+            }
+            else
             {
-                an++;
+                r = md - 1;
             }
         }
         return an;
+    }
+    public bool[] PathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries)
+    {
+        this.N = n;
+        this.Nums = nums;
+        this.MaxDiff = maxDiff;
+        G = new List<int>[n + 1];
+        Parents = new int[n + 1];
+
+        for (int i = 0; i <= n; i++)
+        {
+            Parents[i] = i;
+        }
+
+        for (int i = 0; i <= n; i++)
+        {
+            G[i] = new List<int>();
+        }
+
+        Dsu dsu = new Dsu(N);
+
+        for (int i = 0; i < n; i++)
+        {
+            int ub = UpperBound(nums[i] + maxDiff);
+
+            int pi = dsu.Find(i);
+            int pub = dsu.Find(ub);
+
+            if (pi != pub)
+            {
+                dsu.Union(pi, pub);
+            }
+        }
+
+        int m = queries.Length;
+        bool[] an = new bool[m];
+
+        for (int i = 0; i < m; i++)
+        {
+            int u = queries[i][0];
+            int v = queries[i][1];
+
+            if (dsu.Find(u) == dsu.Find(v)) an[i] = true;
+            else an[i] = false;
+        }
+        return an;
+
     }
 }
 internal class Program
@@ -41,13 +108,13 @@ internal class Program
     {
         Solution solution = new Solution();
 
-        int[] nums = [73, 98, 9];
-        int[][] edges = [[1, 4], [2, 3]];
+        int[] nums = [2, 5, 6, 8];
+        int[][] edges = [[0, 1], [0, 2], [1, 3], [2, 3]];
         bool[] online = [true, true, true, false, true];
 
 
         string s = "z*#";
-        int result = solution.RemoveCoveredIntervals(edges);
+        bool[] result = solution.PathExistenceQueries(4, nums, 2, edges);
         Console.WriteLine(string.Join(", ", result));
         Console.ReadLine();
     }

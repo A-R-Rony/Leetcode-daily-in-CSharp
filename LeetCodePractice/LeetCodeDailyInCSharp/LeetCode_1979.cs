84226065bc08992148210a5934a0eaa84103c1c1
BRACITS\abdur.rony <rhamanrony128@gmail.com>
2026-07-18 11:30:34 +0600

    daily added

---
+++ LeetCodePractice/LeetCodeDailyInCSharp/LeetCode_1979.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCodePractice.LeetCodeDailyInCSharp;
+
+internal class LeetCode_1979
+{
+    public class Solution
+    {
+        public int FindGCD(int[] nums)
+        {
+            Array.Sort(nums);
+            return Gcd(nums[0], nums[^1]);
+        }
+        int Gcd(int a, int b)
+        {
+            if (b == 0) return a;
+            return Gcd(b, a % b);
+        }
+    }
+}
--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -10,27 +10,12 @@ namespace LeetCodePractice;
 
 public class Solution
 {
-    public long GcdSum(int[] nums)
+    public int FindGCD(int[] nums)
     {
-        int n = nums.Length;
-        int[] pre = new int[n];
-        int[] mx = new int[n];
-
-        for (int id = 0; id < n; id++)
-        {
-            mx[id] = int.Max((id-1 >= 0 ? mx[id - 1] : 0), nums[id]);
-            pre[id] = Gcd(mx[id], nums[id]);
-        }
-        Array.Sort(pre);
-        long sm = 0;
-        int i = 0, j = n - 1;
-        while(i < j)
-        {
-            sm += Gcd(pre[i++], pre[j--]);
-        }
-        return sm;
+        Array.Sort(nums);
+        return Gcd(nums[0], nums[^1]);
     }
-    public int Gcd(int a, int b)
+    int Gcd(int a,int b)
     {
         if (b == 0) return a;
         return Gcd(b, a % b);

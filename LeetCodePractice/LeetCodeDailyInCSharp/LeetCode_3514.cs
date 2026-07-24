1dfd6da57fc9c99061fd56919e99d2b7c75ccb41
BRACITS\abdur.rony <rhamanrony128@gmail.com>
2026-07-24 11:28:57 +0600

    daily added

---
+++ LeetCodePractice/LeetCodeDailyInCSharp/LeetCode_3514.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodePractice.LeetCodeDailyInCSharp;
+
+internal class LeetCode_3514
+{
+    public class Solution
+    {
+        public int UniqueXorTriplets(int[] nums)
+        {
+            int n = nums.Length;
+            Dictionary<int, int> cnt = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (cnt.ContainsKey((nums[i] ^ nums[j]))) continue;
+                    cnt[(nums[i] ^ nums[j])] = 1;
+                }
+            }
+            int an = 0;
+            for (int i = 0; i <= 3000; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    int xr = (nums[j] ^ i);
+                    if (cnt.ContainsKey(xr))
+                    {
+                        an++;
+
+                        break;
+                    }
+                }
+            }
+            return an;
+        }
+    }
+}
--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -6,9 +6,31 @@ public class Solution
     public int UniqueXorTriplets(int[] nums)
     {
         int n = nums.Length;
-        if (n <= 2) return n;
-        int bit = (int)Math.Log2(n) + 1;
-        return (1 << bit);
+        Dictionary<int, int> cnt = new Dictionary<int, int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (cnt.ContainsKey((nums[i] ^ nums[j]))) continue;
+                cnt[(nums[i] ^ nums[j])] = 1;
+            }
+        }
+        int an = 0;
+        for (int i = 0; i <= 3000; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                int xr = (nums[j] ^ i);
+                if (cnt.ContainsKey(xr))
+                {
+                    an++;
+                    
+                    break;
+                }
+            }
+        }
+        return an;
     }
 }
 internal class Program
@@ -17,7 +39,7 @@ internal class Program
     {
         Solution solution = new Solution();
         int[][] edges = [[0, 3], [0, 2], [1, 3], [2, 3]];
-        int[] nums = [3, 1, 2];
+        int[] nums = [6, 7, 8, 9];
         int result = solution.UniqueXorTriplets(nums);
         Console.WriteLine(string.Join(", ", result));
 

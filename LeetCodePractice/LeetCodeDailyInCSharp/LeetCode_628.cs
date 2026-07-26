b88c6a393a3ad7add48bd9cc4cee6d9846645867
BRACITS\abdur.rony <rhamanrony128@gmail.com>
2026-07-26 09:40:40 +0600

    daily added

---
+++ LeetCodePractice/LeetCodeDailyInCSharp/LeetCode_628.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCodePractice.LeetCodeDailyInCSharp;
+
+internal class LeetCode_628
+{
+    public class Solution
+    {
+        public int MaximumProduct(int[] nums)
+        {
+            int n = nums.Length;
+            Array.Sort(nums);
+            int an = nums[^1] * nums[^2] * nums[^3];
+            an = int.Max(an, nums[0] * nums[1] * nums[^1]);
+            return an;
+        }
+    }
+}
--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -3,33 +3,12 @@
 
 public class Solution
 {
-    public int UniqueXorTriplets(int[] nums)
+    public int MaximumProduct(int[] nums)
     {
         int n = nums.Length;
-        Dictionary<int, int> cnt = new Dictionary<int, int>();
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (cnt.ContainsKey((nums[i] ^ nums[j]))) continue;
-                cnt[(nums[i] ^ nums[j])] = 1;
-            }
-        }
-        int an = 0;
-        for (int i = 0; i <= 3000; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                int xr = (nums[j] ^ i);
-                if (cnt.ContainsKey(xr))
-                {
-                    an++;
-                    
-                    break;
-                }
-            }
-        }
+        Array.Sort(nums);
+        int an = nums[^1] * nums[^2] * nums[^3];
+        an = int.Max(an, nums[0] * nums[1] * nums[^1]);
         return an;
     }
 }
@@ -40,7 +19,7 @@ internal class Program
         Solution solution = new Solution();
         int[][] edges = [[0, 3], [0, 2], [1, 3], [2, 3]];
         int[] nums = [6, 7, 8, 9];
-        int result = solution.UniqueXorTriplets(nums);
+        int result = solution.MaxProduct(123);
         Console.WriteLine(string.Join(", ", result));
 
         Console.ReadLine();

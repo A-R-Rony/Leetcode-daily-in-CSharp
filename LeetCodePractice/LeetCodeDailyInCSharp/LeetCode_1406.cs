8bb47118591a72c13fe5c399f154377a057b3740
BRACITS\abdur.rony <rhamanrony128@gmail.com>
2026-08-03 10:01:16 +0600

    daily added

---
+++ LeetCodePractice/LeetCodeDailyInCSharp/LeetCode_1406.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCodePractice.LeetCodeDailyInCSharp;
+
+internal class LeetCode_1406
+{
+    public class Solution
+    {
+        int n;
+        int[,] mem;
+        int f(int i, int st, int[] a)
+        {
+            if (i >= n)
+            {
+                return 0;
+            }
+            if (mem[i, st] != -1) return mem[i, st];
+            int d = (st == 1 ? 1 : -1);
+
+            int an = (st == 1 ? int.MinValue : int.MaxValue);
+            an = (st == 1 ? int.Max(an, a[i] + f(i + 1, st ^ 1, a)) : int.Min(an, d * a[i] + f(i + 1, st ^ 1, a)));
+            if (i + 1 < n)
+                an = (st == 1 ? int.Max(an, a[i] + a[i + 1] + f(i + 2, st ^ 1, a)) : int.Min(an, d * (a[i] + a[i + 1]) + f(i + 2, st ^ 1, a)));
+            if (i + 2 < n)
+                an = (st == 1 ? int.Max(an, a[i] + a[i + 1] + a[i + 2] + f(i + 3, st ^ 1, a)) : int.Min(an, d * (a[i] + a[i + 1] + a[i + 2]) + f(i + 3, st ^ 1, a)));
+            return mem[i, st] = an;
+        }
+        public string StoneGameIII(int[] stoneValue)
+        {
+            n = stoneValue.Length;
+            mem = new int[n + 1, 2];
+            for (int i = 0; i < n; i++)
+            {
+                mem[i, 0] = mem[i, 1] = -1;
+            }
+            int an = f(0, 1, stoneValue);
+            if (an > 0) return "Alice";
+            else if (an < 0) return "Bob";
+            return "Tie";
+        }
+    }
+
+}
--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -4,40 +4,35 @@ public class Solution
 {
     int n;
     int[,] mem;
-    int f(int i, int j, int st, int[] piles)
+    int f(int i, int st, int[] a)
     {
-        if (i > j || i == n || j == -1) return 0;
-        
-        int d = (st == 0 ? -1 : 1);
-        int an = 0;
-        if (mem[i, j] != -1) return mem[i, j];
-
-        if (st == 1)
+        if (i >= n)
         {
-            an = f(i + 1, j, st ^ 1, piles) + piles[i];
-            an = int.Max(an, piles[j] + f(i, j - 1, st ^ 1, piles));
+            return 0;
         }
-        else
-        {
-            an = -piles[i] + f(i + 1, j, st ^ 1, piles);
-            an = int.Min(an, piles[j] + f(i, j - 1, st ^ 1, piles));
-        }
-        return mem[i,j] = an;
+        if (mem[i, st] != -1) return mem[i, st];
+        int d = (st == 1 ? 1 : -1);
 
+        int an = (st == 1 ? int.MinValue : int.MaxValue);
+        an = (st == 1 ? int.Max(an, a[i] + f(i + 1, st ^ 1, a)) : int.Min(an, d * a[i] + f(i + 1, st ^ 1, a)));
+        if (i + 1 < n)
+            an = (st == 1 ? int.Max(an, a[i] + a[i + 1] + f(i + 2, st ^ 1, a)) : int.Min(an, d * (a[i] + a[i + 1]) + f(i + 2, st ^ 1, a)));
+        if (i + 2 < n)
+            an = (st == 1 ? int.Max(an, a[i] + a[i + 1] + a[i + 2] + f(i + 3, st ^ 1, a)) : int.Min(an, d * (a[i] + a[i + 1] + a[i + 2]) + f(i + 3, st ^ 1, a)));
+        return mem[i, st] = an;
     }
-    public bool StoneGame(int[] piles)
+    public string StoneGameIII(int[] stoneValue)
     {
-        n = piles.Length;
-        mem = new int[n + 1, n + 1];
+        n = stoneValue.Length;
+        mem = new int[n + 1, 2];
         for (int i = 0; i < n; i++)
         {
-            for (int j = 0; j < n; j++)
-            {
-                mem[i, j] = -1;
-            }
+            mem[i, 0] = mem[i, 1] = -1;
         }
-        return (f(0, n - 1, 1, piles) > 0 ? true : false);
-
+        int an = f(0, 1, stoneValue);
+        if (an > 0) return "Alice";
+        else if (an < 0) return "Bob";
+        return "Tie";
     }
 }
 internal class Program
@@ -46,8 +41,8 @@ internal class Program
     {
         Solution solution = new Solution();
         int[][] edges = [[0, 3], [0, 2], [1, 3], [2, 3]];
-        int[] nums = [7, 7, 12, 16, 41, 48, 41, 48, 11, 9, 34, 2, 44, 30, 27, 12, 11, 39, 31, 8, 23, 11, 47, 25, 15, 23, 4, 17, 11, 50, 16, 50, 38, 34, 48, 27, 16, 24, 22, 48, 50, 10, 26, 27, 9, 43, 13, 42, 46, 24];
-        bool result = solution.StoneGame(nums);
+        int[] nums = [1, 2, 3, -9];
+        var result = solution.StoneGameIII(nums);
         Console.WriteLine(string.Join(", ", result));
 
         ///Summary

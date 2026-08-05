648683225baa3167a8d9d36a721a410c828c91fd
BRACITS\abdur.rony <rhamanrony128@gmail.com>
2026-08-05 09:38:17 +0600

    daily added

---
+++ LeetCodePractice/LeetCodeDailyInCSharp/LeetCode_3310.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCodePractice.LeetCodeDailyInCSharp;
+
+internal class LeetCode_3310
+{
+    public class Solution
+    {
+        List<int>[] g;
+        int[] vis;
+        void Dfs(int nd)
+        {
+            vis[nd] = 1;
+            foreach (var child in g[nd])
+            {
+                if (vis[child] == 1) continue;
+                Dfs(child);
+            }
+        }
+        public IList<int> RemainingMethods(int n, int k, int[][] invocations)
+        {
+            vis = new int[n + 1];
+            HashSet<int> an = new();
+
+            for (int i = 0; i < n; i++) vis[i] = 0;
+
+            g = new List<int>[n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                g[i] = new();
+            }
+            foreach (var invocation in invocations)
+            {
+                g[invocation[0]].Add(invocation[1]);
+            }
+
+            Dfs(k);
+            foreach (var e in invocations)
+            {
+                int u = e[0], v = e[1];
+                if (vis[u] == 0 && vis[v] == 1)
+                {
+                    return Enumerable.Range(0, n).ToList();
+                }
+            }
+
+            List<int> ans = new();
+            for (int i = 0; i < n; i++)
+            {
+                if (vis[i] == 0)
+                    ans.Add(i);
+            }
+            return ans;
+        }
+    }
+}
--- LeetCodePractice/Program.cs
+++ LeetCodePractice/Program.cs
@@ -2,37 +2,51 @@
 
 public class Solution
 {
-    int n;
-    int[,] mem;
-    int f(int i, int st, int[] a)
+    List<int>[] g;
+    int[] vis;
+    void Dfs(int nd)
     {
-        if (i >= n)
+        vis[nd] = 1;
+        foreach (var child in g[nd])
         {
-            return 0;
+            if (vis[child] == 1) continue;
+            Dfs(child);
         }
-        if (mem[i, st] != -1) return mem[i, st];
-        int d = (st == 1 ? 1 : -1);
-
-        int an = (st == 1 ? int.MinValue : int.MaxValue);
-        an = (st == 1 ? int.Max(an, a[i] + f(i + 1, st ^ 1, a)) : int.Min(an, d * a[i] + f(i + 1, st ^ 1, a)));
-        if (i + 1 < n)
-            an = (st == 1 ? int.Max(an, a[i] + a[i + 1] + f(i + 2, st ^ 1, a)) : int.Min(an, d * (a[i] + a[i + 1]) + f(i + 2, st ^ 1, a)));
-        if (i + 2 < n)
-            an = (st == 1 ? int.Max(an, a[i] + a[i + 1] + a[i + 2] + f(i + 3, st ^ 1, a)) : int.Min(an, d * (a[i] + a[i + 1] + a[i + 2]) + f(i + 3, st ^ 1, a)));
-        return mem[i, st] = an;
     }
-    public string StoneGameIII(int[] stoneValue)
+    public IList<int> RemainingMethods(int n, int k, int[][] invocations)
     {
-        n = stoneValue.Length;
-        mem = new int[n + 1, 2];
+        vis = new int[n + 1];
+        HashSet<int> an = new();
+
+        for (int i = 0; i < n; i++) vis[i] = 0;
+
+        g = new List<int>[n + 1];
         for (int i = 0; i < n; i++)
         {
-            mem[i, 0] = mem[i, 1] = -1;
+            g[i] = new();
         }
-        int an = f(0, 1, stoneValue);
-        if (an > 0) return "Alice";
-        else if (an < 0) return "Bob";
-        return "Tie";
+        foreach (var invocation in invocations)
+        {
+            g[invocation[0]].Add(invocation[1]);
+        }
+
+        Dfs(k);
+        foreach (var e in invocations)
+        {
+            int u = e[0], v = e[1];
+            if (vis[u] == 0 && vis[v] == 1)
+            {
+                return Enumerable.Range(0, n).ToList();
+            }
+        }
+
+        List<int> ans = new();
+        for (int i = 0; i < n; i++)
+        {
+            if (vis[i] == 0)
+                ans.Add(i);
+        }
+        return ans;
     }
 }
 internal class Program
@@ -40,9 +54,9 @@ internal class Program
     static void Main(string[] args)
     {
         Solution solution = new Solution();
-        int[][] edges = [[0, 3], [0, 2], [1, 3], [2, 3]];
+        int[][] edges = [[0, 1], [1, 2], [2, 3]];
         int[] nums = [1, 2, 3, -9];
-        var result = solution.StoneGameIII(nums);
+        var result = solution.RemainingMethods(4, 3, edges);
         Console.WriteLine(string.Join(", ", result));
 
         ///Summary
